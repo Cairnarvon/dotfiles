e82ea3d6c34f91b5fa7b4af3a9dcd3a9739e1b80
Cairnarvon <cairnarvon@gmail.com>
2011-11-16 15:44:13 +0100

    More Canto config fiddling.

--- .canto/conf.py
+++ .canto/conf.py
@@ -16,7 +16,7 @@ keys['2'] = set_filter(None)
 keys['/'] = search_filter
 keys['y'] = yank
 
-reader_keys['w'] = wget_link("/home/cairnarvon/wgotten")
+reader_keys['w'] = wget_link("%s/wgotten" % os.getenv("HOME"))
 
 def resize_hook(cfg):
     cfg.reader_orientation = "right"
@@ -33,27 +33,13 @@ default_rate(60)
 # Rota Hall
 add("http://cairnarvon.rotahall.org/feed/",     tags=[None, "Rota Hall"])
 add("http://terras.rotahall.org/?feed=rss2",    tags=[None, "Rota Hall"])
-add("http://mai.rotahall.org/?feed=rss2",       tags=["Maia", "Rota Hall"])
-add("http://www.mcgilleus.ca/blog/11/feed",     tags=["Maia", "Rota Hall"])
+add("http://citywomenwithdolls.tumblr.com/rss", tags=[None, "Rota Hall"])
 add("http://coren.rotahall.org/?feed=rss2",     tags=[None, "Rota Hall"])
 add("http://saythings.rotahall.org/?feed=rss2", tags=[None, "Rota Hall"])
-add("http://ninja.rotahall.org/?feed=rss2",     tags=[None, "Rota Hall"])
 add("http://qow.rotahall.org/?feed=rss2",       tags=[None, "Rota Hall"])
 
-# RL
-add("http://menoname.wordpress.com/feed/")
-add("http://blog.associatie.kuleuven.be/tinekemps/feed/")
-
-# Pharyngula and #pharyngula
-add("http://scienceblogs.com/pharyngula/index.xml",             tags=["Pharyngula"])
-add("http://feeds.feedburner.com/freethoughtblogs/pharyngula",  tags=["Pharyngula"])
-add("http://scienceonlooker.blogspot.com/rss.xml", tags=["cactaur"])
-add("http://missivesfromthefrontallobe.blogspot.com/feeds/posts/default?alt=rss",
-    tags=["klbadger"])
-add("http://wockets.org/?feed=rss", tags=["Nerull"])
-add("http://greedygreedyalgorithms.blogspot.com/feeds/posts/default?alt=rss")
-
-# Other ScienceBlogs and science blogs and things
+# ScienceBlogs and science blogs and things
+add("http://feeds.feedburner.com/freethoughtblogs/pharyngula")
 add("http://scienceblogs.com/erv/index.xml")
 add("http://whyevolutionistrue.wordpress.com/feed/")
 add("http://scienceblogs.com/deltoid/index.xml")
@@ -65,10 +51,8 @@ add("http://blog.desudesudesu.org/?feed=rss2",  tags=[None, "/prog/"])
 add("http://j.rigelseven.com/rsst.xml",         tags=[None, "/prog/"])
 add("http://leahculver.com/feed/",              tags=[None, "/prog/"])
 add("http://collison.ie/blog/feed",             tags=[None, "/prog/"])
-add("http://feeds.feedburner.com/AngrySummerQuietFall", tags=["Shii"])
 
 # Misc. Internets
-add("http://blog.myspace.com/blog/rss.cfm?friendID=18382968", tags=["Laura"])
 add("http://alterisego.wordpress.com/feed/")
 add("http://feeds.feedburner.com/andyfreeland")
 add("http://headinjurytheater.blogspot.com/feeds/posts/default?alt=rss")
@@ -83,6 +67,7 @@ add("http://pentadecathlon.com/lifeNews/index.xml")
 add("http://feeds.feedburner.com/lbrandy")
 add("http://feeds.feedburner.com/schneier/fulltext")
 add("http://rijmenants.blogspot.com/feeds/posts/default?alt=rss")
+add("http://greedygreedyalgorithms.blogspot.com/feeds/posts/default?alt=rss")
 
 # People still make webcomics for some reason
 add("http://www.rsspect.com/rss/qwantz.xml")
@@ -90,6 +75,5 @@ add("http://www.smbc-comics.com/rss.php")
 add("http://oglaf.com/feeds/rss/")
 
 # Misc.
-add("http://www.languagehat.com/index.rdf")
 add("http://feeds.arstechnica.com/arstechnica/BAaf")
 add("http://richarddawkins.net/feed.php")
